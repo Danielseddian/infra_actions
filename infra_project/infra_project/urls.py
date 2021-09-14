da4765895f29db7435f97aeb586775b65b3a3a0b
Кучин Денис <danielseddian@yandex.ru>
2021-09-14 23:55:42 +0200

    2021-09-14_23-55

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
-
 urlpatterns = [
     path("", include("infra_app.urls", namespace="infra_app")),
     path("admin/", admin.site.urls),
