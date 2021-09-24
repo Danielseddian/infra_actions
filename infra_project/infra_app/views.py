fc8d0861983b4a59b93b67ba4c26ea1eca2ba17a
Кучин Денис <danielseddian@yandex.ru>
2021-09-25 02:29:32 +0300

    2021-09-25 02-30

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Ура! Наконец-то, у меня получилось!")
+    return HttpResponse("Ну, наконец-то, у меня получилось!")
 
 
 def second_page(request):
